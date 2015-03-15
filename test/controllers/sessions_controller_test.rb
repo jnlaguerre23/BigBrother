fc5996ecdb646262f75a1f5ef8d842e64173818d
jonathan laguerre <jonathan.n.laguerre@gmail.com>
2015-03-15 16:41:07 -0400

    delete

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionController::TestCase
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+
+end
