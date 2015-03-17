412b5a416f691aaee0454a9cd7e0ddef0a9ce6d7
jonathan laguerre <jonathan.n.laguerre@gmail.com>
2015-03-17 01:28:55 -0400

    password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,13 +65,14 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'mighty-thicket-2813.herokuapp.com' }
+  host = 'mighty-thicket-2813.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => 'myemail@gmail.com',
-    :password       => 'Gmailpasswordhere',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,13 @@
 Rails.application.routes.draw do
-
-  
-
-  root 'static_pages#home'
-  get 'home' => 'static_pages#home'
-  get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'
+  root                'static_pages#home'
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-
-  get 'help' => 'static_pages#help'
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
-
-
-
 end
