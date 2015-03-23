53794c5a0c42892771acc5c94b7d32b5841a9b62
jonathan laguerre <jonathan.n.laguerre@gmail.com>
2015-03-23 16:25:23 -0400

    cmit

--- app/models/user.rb
+++ app/models/user.rb
@@ -74,8 +74,5 @@ private
     self.email = email.downcase
   end
 
-  def create_activation_digest
-    self.activation_token = User.new_token
-    self.activation_digest = User.digest(activation_token)
-  end
+
 end
