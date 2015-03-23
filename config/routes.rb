ee2024e37dd6a8ddac035dff7a7ca1cd5edf3362
jonathan laguerre <jonathan.n.laguerre@gmail.com>
2015-03-22 23:25:09 -0400

    validate

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   get    'about'   => 'static_pages#about'
   get    'contact' => 'static_pages#contact'
   get    'signup'  => 'users#new'
+  get 'account_activation' => 'account_activations#edit', :as => :edit_account_activation
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
-  resources :account_activations, only: [:edit]
+  #resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
 end
