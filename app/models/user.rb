d9b291ff7f068cc0c7c53b96fa1c13ca3a90c878
jonathan laguerre <jonathan.n.laguerre@gmail.com>
2015-03-23 16:41:28 -0400

    com

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
-  #before_create :create_activation_digest
+  before_create :create_activation_digest
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255},
