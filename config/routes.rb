a2a031856003978c905823f545ec3804e8e6242c
jonathan laguerre <jonathan.n.laguerre@gmail.com>
2015-03-22 23:34:59 -0400

    app controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
-  #resources :account_activations, only: [:edit]
+  resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
 end
