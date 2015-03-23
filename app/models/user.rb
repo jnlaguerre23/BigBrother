a573de304df2e0cf03bbe6ad5408994921a96c30
jonathan laguerre <jonathan.n.laguerre@gmail.com>
2015-03-23 00:35:01 -0400

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,7 +41,7 @@ class User < ActiveRecord::Base
   end
 
   def authenticated?(attribute, token)
-    digest = self.send("#{attribute}_digest")
+    digest = send("#{attribute}_digest")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
