d67de720cd34ebf8f0046bc754846255a6171a6a
jonathan laguerre <jonathan.n.laguerre@gmail.com>
2015-03-23 18:17:35 -0400

    commit

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module BigBrother
+module Rails
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    #config.filters = false
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -25,6 +24,7 @@ module BigBrother
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.filter_parameters += [:password]
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
 
