a86b3f6d6d2692298ce09f06f48d35c52d1e64c2
jonathan laguerre <jonathan.n.laguerre@gmail.com>
2015-03-23 18:07:54 -0400

    commit

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
+##require "active_record/railtie"
+##require "action_controller/railtie"
+##require "action_mailer/railtie"
 #require "active_resource/railtie"
 # require "rails/test_unit/railtie"
-require "sprockets/railtie"
+##require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -16,6 +16,7 @@ module BigBrother
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #config.filters = false
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -27,6 +28,6 @@ module BigBrother
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
 
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,6 @@ Rails.application.configure do
    # Code is not reloaded between requests.
    config.cache_classes = true
 
-   config.filters = false
    # Eager load code on boot. This eager loads most of Rails and
    # your application in memory, allowing both thread web servers
    # and those relying on copy on write to perform better.
