8b979fe5ec26d3ce00737ea3a5baa4dafaffd17b
jonathan laguerre <jonathan.n.laguerre@gmail.com>
2015-03-25 23:59:31 -0400

    email validate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
     root                'static_pages#home'
     get    'help'    => 'static_pages#help'
     get    'about'   => 'static_pages#about'
@@ -10,4 +11,22 @@ Rails.application.routes.draw do
     resources :users
     resources :account_activations, only: [:edit]
     resources :password_resets,     only: [:new, :create, :edit, :update]
+=======
+
+  get 'sessions/new'
+
+  get 'users/new'
+
+  root 'static_pages#home'
+  get 'home' => 'static_pages#home'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
+  resources :users
+
+>>>>>>> 1c780629c04a3abffaf8552812561386773c3ea1
 end
