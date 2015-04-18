0ac75eabfe97222f00939847903e338afab53471
jonathan laguerre <jonathan.n.laguerre@gmail.com>
2015-04-18 12:51:36 -0400

    add columns to student

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -9,6 +9,10 @@ class StudentController < ApplicationController
     @user = User.find(session[:user_id])
     @student.user=@user
     @student.school= params[:student][:school]
+    @student.phone = params[:student][:phone]
+    @student.grade = params[:student][:grade]
+    @student.gpa = params[:student][:gpa]
+    @student.nationality = params[:student][:nationality]
     if @student.save
       flash[:info] = "Successfully applied"
       redirect_to root_url
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150415005128) do
+ActiveRecord::Schema.define(version: 20150418164159) do
 
   create_table "students", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "school"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "phone"
+    t.integer  "grade"
+    t.integer  "gpa"
+    t.string   "nationality"
   end
 
   add_index "students", ["user_id"], name: "index_students_on_user_id"
