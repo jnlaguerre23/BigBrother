d2b7052557307ef6e582935d563f6b413e2e5d3d
Levon Lloyd <levon.lloyd@gmail.com>
2015-04-18 11:57:15 -0400

    fix up merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150414072300) do
+ActiveRecord::Schema.define(version: 20150415005128) do
+
+  create_table "students", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "school"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "students", ["user_id"], name: "index_students_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
