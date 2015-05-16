319b241c263d4f8cb28a5fafd5d0aec3155d9279
jonathan laguerre <jonathan.n.laguerre@gmail.com>
2015-05-16 13:57:51 -0400

    changed fb_login appid

--- app/controllers/mentor_controller.rb
+++ app/controllers/mentor_controller.rb
@@ -9,13 +9,14 @@ class MentorController < ApplicationController
     @mentor=Mentor.new
     @user = User.find(session[:user_id])
     @mentor.user=@user
-    @mentor.email=@user.email
+    @mentor.email=@mentor.user.email
     @mentor.school= params[:mentor][:school]
     @mentor.major = params[:mentor][:major]
     @mentor.graduation_year = params[:mentor][:graduation_year]
     if @mentor.save
-      flash[:info] = "Successfully applied"
-      redirect_to root_url
+      flash[:info] = "Successfully applied as a mentor"
+      redirect_to users_path
+      #redirect_to root_url
     else
       render 'new'
     end
--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,5 +1,6 @@
 class Mentor < ActiveRecord::Base
   belongs_to :user
+  has_many  :students, dependent: :destroy
   validates :school, presence:true
   validates :graduation_year, :numericality => { :greater_than_or_equal_to => 0 }
   validates :graduation_year, presence:true
