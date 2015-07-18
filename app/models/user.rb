7a19f999432eba5bcb6af747ebe908c99e4b7078
Arthur Thornton <arthur@arthurthornton.net>
2015-07-18 14:25:02 -0700

    Use self, not User, in method names

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,14 +14,14 @@ class User < ActiveRecord::Base
 
   has_many :microposts, dependent: :destroy
 
-    # Returns the hash digest of the given string.
-  def User.digest(string)
+  # Returns the hash digest of the given string.
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
-  def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
 
